769f021112446888029062c84772a4baca05c55e
asus0007 <asus0007@student.monash.edu>
2023-10-13 23:40:04 +0800

    update person.csv

--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
-node = pd.read_csv("ACCIDENT.csv")
-print(node["Accident Type Desc"].unique())
+node = pd.read_csv("PERSON.csv")
+print(node["Age Group"].unique())
+print(node["SEX"].unique())
+print(node["Inj Level Desc"].unique())
+print(node["Road User Type Desc"].unique())
+print(node["SEATING_POSITION"].unique())
+
+
 
 # Group by "Light Condition Desc" and "Road Geometry Desc" and count the accidents
-# grouped_data = node.groupby(['Region Name']).agg({'Lat': 'mean', 'Long': 'mean'}).reset_index()
+# grouped_data = node.groupby(['Road User Type Desc', "Inj Level Desc"]).agg({'Lat': 'mean', 'Long': 'mean'}).reset_index()
+grouped_data = node.groupby(['Age Group', "Inj Level Desc"])["ACCIDENT_NO"].count().reset_index()
 
 # # Display the result
-# print(grouped_data)
+print(grouped_data)
+# print(grouped_data[(grouped_data["Road User Type Desc"] == "Drivers") | (grouped_data["Road User Type Desc"] == "Passengers")])
 
  
  
